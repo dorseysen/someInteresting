e0436ce6053148369a110640d2ab56b5bf0d873f
dorseysen <1092240376@qq.com>
2019-01-31 20:08:17 +0800

    修复部分bug

--- musicPlayerNew/js/module/musicBox.js
+++ musicPlayerNew/js/module/musicBox.js
@@ -6,7 +6,10 @@ const MusicBox = {
     init () {
         this.index = 0;
         this.musicNumber = MusicConfig.url.length;
-        this.variousListenerEvents();
+        // this.slideAble = false;
+
+        this.variousListenerEvents();   //添加各类事件监听
+        this.adjustProgress();  // 增加鼠标拖拽
     },
     next () {
         this.index ++;
@@ -49,7 +52,33 @@ const MusicBox = {
         time<10?time="0"+time:time;
         return time;
     },
-
+    adjustProgress () {
+        let _self = this;
+        /*监听鼠标拖拽*/
+        $(".barSlide").on("mousedown",function(e){
+            e=e||window.event;
+            _self.slideAble=true;
+            var ex=e.pageX-$(".barSlide").get(0).offsetLeft;
+            //每一次点击都存储一个滑块x初值，由于这里滑块得保持水平，所以不加一个Y，
+            // 这里各位可以延伸写一个拖拽组件出来
+            $(document).on({
+                mousemove:function(e){
+                    e=e||window.event;
+                    if(_self.slideAble){
+                        var l=e.pageX-ex; 
+                        l > $(".barBg").width()-1 ? l = $(".barBg").width() - 1 : l; //判断拖拽条是否超界
+                        l < 0 ? l = 0 : l;
+                        $("#music audio").get(0).currentTime = (l / $(".barBg").width()) * $("#music audio").get(0).duration;
+                        $(".barFg").css("width",l + "px");
+                        $(".barSlide").css("left",l + "px");
+                    }
+                },
+                mouseup:function(){
+                    _self.slideAble=false;
+                }
+            });
+        });
+    },
 
     variousListenerEvents () {
 
@@ -62,7 +91,7 @@ const MusicBox = {
 
         //按钮组点击
         $(".btnGroup").on("click",".playBtn", function () {
-            _self.play();
+            _self.play(_self.index);
         });
         $(".btnGroup").on("click",".pauseBtn", function () {
             _self.pause();
@@ -84,24 +113,22 @@ const MusicBox = {
         $("#music audio").on("timeupdate",function(){
             let min,sec,minAll="00",secAll="00";
             /*歌曲当前播放进度*/
-            min=_self.timeHandle(this.currentTime,"minute");//调用dorseyHf的时间处理函数，算是比较简单的封装吧
+            min=_self.timeHandle(this.currentTime,"minute");
             sec=_self.timeHandle(this.currentTime,"second");
             /*歌曲播放总时长*/
             if(this.duration){
-                //为什么用if呢？你可以去掉试试，因为当我们切换上下首时，duration监听不到，是一个undefined,会使计算的结果变成NaN，
-                // 这样我们的界面也会看到这个NaN（计算的结果非数字值），这样就不好了
+                
                 minAll=_self.timeHandle(this.duration,"minute");
                 secAll=_self.timeHandle(this.duration,"second");
             }
             /*播放进度条*/
             let allW=$(".barBg").outerWidth(),slideW;
-            slideW=allW*this.currentTime/this.duration;//当前播放进度时间除以总歌曲持续时间
+            slideW=allW*this.currentTime/this.duration;
             $(".barFg").css("width",slideW+"px");
             $(".barSlide").css("left",slideW+"px");
             $(".time").children('span').eq(1).html(min+":"+sec+"/"+minAll+":"+secAll);
             //播放完毕自动播放下一首
             if(this.currentTime>=this.duration-1){
-                
                 _self.next();
             }
         });
